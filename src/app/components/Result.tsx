24b041acea8554231eb7ab18f487131244d69858
Konrad Gabrys <mprx345@gmail.com>
2024-04-01 22:26:04 +0200

    feat/fixed bg color and unit placement

--- src/app/components/Result.tsx
+++ src/app/components/Result.tsx
@@ -6,10 +6,10 @@ interface ResultProps {
 }
 
 export default function Results({ children, id, value, unit }: ResultProps) {
-    const inputStyle = "bg-gray-50 text-center text-xl font-bold text-gray-900 dark:bg-gray-700 dark:text-white"
+    const inputStyle = "text-center text-xl font-bold dark:text-white"
 
     return (
-        <div className="flex flex-col p-1 w-96 items-center rounded-2xl bg-gray-700">
+        <div className="flex flex-col p-1 w-96 items-center rounded-2xl bg-gray-900">
             <label className="block mb-0.5 text-sm font-medium text-gray-400">{children}</label>
             {id !== 'weeks' && id !== 'endDate' && <p id={id} className={inputStyle}>{value} {unit}</p>}
             {(id === 'weeks' || id === 'endDate') && <p id={id} className={inputStyle}>{value}</p>}
