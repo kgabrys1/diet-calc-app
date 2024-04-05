55554a9fe0bc24667fca8e3cb2a1a266ea9cbcd7
Konrad Gabrys <mprx345@gmail.com>
2024-04-05 09:37:47 +0200

    fix/code refactoring

--- src/app/components/Result.tsx
+++ src/app/components/Result.tsx
@@ -1,5 +1,8 @@
 import { Tooltip } from 'react-tooltip'
 
+
+const pStyle = "text-center text-xl font-bold dark:text-white"
+
 interface ResultProps {
     children: React.ReactNode
     id: string
@@ -9,23 +12,20 @@ interface ResultProps {
 }
 
 export default function Results({ children, id, value, tooltipText, unit }: Readonly<ResultProps>) {
-    const pStyle = "text-center text-xl font-bold dark:text-white"
-    const isWeeks = id === 'weeks'
-    const isEndDate = id === 'endDate'
-    const isCi = id === 'ci'
     return (
-        <div
-            className="flex flex-col p-1 w-96 items-center rounded bg-gray-900"
-            data-tooltip-id={`tooltip-${id}`}
-            data-tooltip-content={tooltipText}
-            data-tooltip-place="top"
-        >
-            <label className="block mb-0.5 text-sm font-medium text-gray-400">{children}</label>
-            {!isWeeks && !isEndDate && <p id={id} className={pStyle}>{value} {unit}</p>}
-            {(isWeeks || isEndDate) && <p id={id} className={pStyle}>{value}</p>}
-            {isCi &&
+        <div className="flex flex-col p-1 w-96 items-center rounded bg-gray-900">
+            <label
+                className={`block mb-0.5 text-sm font-medium text-gray-400 ${tooltipText && 'hover:text-gray-300'}`}
+                data-tooltip-id={`tooltip-${id}`}
+                data-tooltip-content={tooltipText}
+                data-tooltip-place="top">
+                {children}
+            </label>
+            {unit && <p id={id} className={pStyle}>{value} {unit}</p>}
+            {!unit && <p id={id} className={pStyle}>{value}</p>}
+            {tooltipText &&
                 <Tooltip style={{ width: "25%" }} id={`tooltip-${id}`} />
             }
-        </div>
+        </div >
     )
 }
